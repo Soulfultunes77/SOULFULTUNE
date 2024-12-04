de37fd1f5f6be533058375b30609a2484ffecaaf
Soulfultunes77 <soulfultunes77@gmail.com>
2024-12-04 06:25:33 +0530

    Update home

---
+++ home/style.js
@@ -0,0 +1,23 @@
+// Open Left Sidebar Menu
+function openLeftMenu() {
+    const leftMenu = document.getElementById('leftMenu');
+    leftMenu.style.display = 'flex'; // Show menu
+}
+
+// Close Left Sidebar Menu
+function closeLeftMenu() {
+    const leftMenu = document.getElementById('leftMenu');
+    leftMenu.style.display = 'none'; // Hide menu
+}
+
+// Open Right Sidebar Menu
+function openRightMenu() {
+    const rightMenu = document.getElementById('rightMenu');
+    rightMenu.style.display = 'flex'; // Show menu
+}
+
+// Close Right Sidebar Menu
+function closeRightMenu() {
+    const rightMenu = document.getElementById('rightMenu');
+    rightMenu.style.display = 'none'; // Hide menu
+}
